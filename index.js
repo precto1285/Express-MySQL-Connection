3824e5a08a1df2832de1008e5e5719216ffb7fe2
Philippe Recto <precto1285@gmal.com>
2019-04-07 10:14:12 -0700

    created const app and made available on localhost:4000

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+
+const app = express();
+
+app.use(cors());
+
+app.listen(4000, console.log('Listening to APP on PORT 4000'));
