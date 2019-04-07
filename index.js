b2826887c0973cd1175301f5165ac0cea795e716
Philippe Recto <precto1285@gmal.com>
2019-04-07 10:19:46 -0700

    connected mysql db to index.js

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'foodindex_db'
+});
+
+connection.connect((err) => {
+  if (err) {
+    return err;
+  }
+});
+
 const app = express();
 
 app.use(cors());
